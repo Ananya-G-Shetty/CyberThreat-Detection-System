18858205fc0b79e78d0180dfa3b294df1a2857e8
AnanyaShetty <ananyashetty925@gmail.com>
2026-05-24 00:30:38 +0530

    Configure Flask for deployment

--- TM_backend/app.py
+++ TM_backend/app.py
@@ -1708,4 +1708,7 @@ if __name__ == '__main__':
     print("=" * 50)
     
     # Run the Flask app
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000) old line with debug=True
+
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
